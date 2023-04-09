a7a86270f146528d5bee7bc92d2692ae37e607f1
iYoung Man <yao.zo@northeastern.edu>
2023-04-09 13:06:07 -0700

    Hw9 (#59)
    * resolve conflict11
    
    * resolve conflict12
    
    * resolve conflict14
    
    * resolve conflict16
    
    * resolve conflict17
    
    * cd1
    
    * cd2
    
    * cd3
    
    * commmit
    
    * commmit
    
    * de5
    
    * cd6
    
    * cd6
    
    * done
    
    * done2
    
    * add aws-infra
    
    * add aws-infra
    
    * make some changes
    
    * make some changes
    
    * fix some bug
    
    * change to pkr.hcl
    
    * currently good
    
    * stag4
    
    * success now
    
    * finish most part
    
    * allala
    
    * finsh hw5
    
    * finsih hw5
    
    * finish hw5
    
    * hw5-v3
    
    * hw5-v3
    
    * add1
    
    * testhw5
    
    * testhw5
    
    * finish hw6
    
    * Assignment 7
    
    * assignment 7
    
    * assignment 7
    
    * hw7
    
    * current change
    
    * assignment 8
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9
    
    * hw9

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -8,7 +8,8 @@ module "us-west-2-vpc1" {
   public_subnets_num  = 3
   private_subnets_num = 3
   availability_zones  = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  ami-id              = "ami-038f39e8d0e91d650"
+#  ami-id              = "ami-038f39e8d0e91d650"
+  ami-id =            "ami-0216d05cafe7d23d9"
   db-username         = "csye6225"
   db-password         = "wu390u3j3b3dn"
   db-name             = "csye6225"
